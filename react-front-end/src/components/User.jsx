b3f730065e28a54c26a76fc5387671b0653d2d03
Dennis Wong <denniswong0106@gmail.com>
2021-05-07 21:46:57 +0000

    small change user.jsx

--- react-front-end/src/components/User.jsx
+++ react-front-end/src/components/User.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import NavBar from './NavBar';
+import React from "react";
+import NavBar from "./NavBar";
 
+// if user props passed in - has no user
+// redirect back to homepage
+// if user props passed in - has user
+// display user info
+
+// hardcode user data in props:
+
+const props = {
+  user: {
+    name: "Joe the builder",
+    email: "bobsfriend@builders.com",
+    bank_account: "0000-0000-0000-0000",
+    daily_prize: false,
+  },
+};
 
 const User = () => {
-    return (
-        <div>
-            <NavBar/>
-            <h3>User!</h3>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <h3>User!</h3>
+    </div>
+  );
 };
 
 export default User;
