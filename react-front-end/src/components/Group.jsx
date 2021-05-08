1feb4a13c242bacae2aefff4d58f4f23ef75b651
Ted <ted@ted-ubuntu-desktop>
2021-05-07 20:32:27 -0600

    add group-trip page

---
+++ react-front-end/src/components/Group.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import NavBar from './NavBar';
+import LinearProgressWithLabel from "./LinearProgressWithLabel";
+import "./Group.scss"
+
+const Group = (props) => {
+
+  const today = new Date();
+  const currentDay = today.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
+
+  return(
+    <>
+      <NavBar />
+      <div className="group-title">
+        <h1>Iceland 2077!</h1>
+        <h2>Date: {currentDay}</h2>
+        <h4>Only 364 days to go!</h4>
+      </div>
+
+      <div>
+        <h1>Progress:</h1>
+      </div>
+      <div className="group-progress">
+        <img className="avatar-img" alt="rex" src="https://i.imgur.com/LpaY82x.png" />
+        <h4>Rex Raptor</h4>
+        <LinearProgressWithLabel value={55} />
+      </div>
+
+    </>
+  );
+}
+
+export default Group;
