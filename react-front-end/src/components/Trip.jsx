1719915b0e0a1e4044d88dcffd27a0dfccbb7ed2
P-Hansen <philip.pt.hansen@gmail.com>
2021-05-07 19:26:58 -0600

    pop up w slider

--- react-front-end/src/components/Trip.jsx
+++ react-front-end/src/components/Trip.jsx
@@ -4,7 +4,50 @@ import './Trip.scss';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 
+//popup
+import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+//slider
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
 const Trip = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const useStyles = makeStyles({
+        root: {
+          width: 300,
+        },
+      });
+
+      function valuetext(value) {
+        return `${value}°C`;
+      }
+
+      const marks = [
+        {
+          value: 1,
+        },
+        {
+          value: 5,
+        },
+        {
+          value: 10,
+        },
+      ];
+
     return (
         <div>
             <NavBar/>
@@ -18,10 +61,48 @@ const Trip = () => {
                     <Card class="price-card">
                         <h5>5 days</h5>
                         <h2>$6522</h2>
-                        <Button size="medium">Book now</Button>
+                        <Button size="medium" onClick={handleClickOpen}>Book now</Button>
                     </Card>
                 </div>
             </div>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Book your trip</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Trip name"
+                        fullWidth
+                    />
+                    <h3>
+                        Iceland $6522
+                    </h3>
+                    <Typography id="discrete-slider" gutterBottom>
+                        Temperature
+                    </Typography>
+                    <Slider
+                        defaultValue={1}
+                        getAriaValueText={valuetext}
+                        marks={marks}
+                        aria-label="ios slider"
+                        // aria-labelledby="discrete-slider"
+                        valueLabelDisplay="auto"
+                        step={0.25}
+                        marks
+                        min={1}
+                        max={10}
+                    />
+                </DialogContent>
+                <div class="dialog-actions">
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button id="start" onClick={handleClose} color="primary">
+                        Start Saving
+                    </Button>
+                </div>
+            </Dialog>
         </div>
     );
 };
