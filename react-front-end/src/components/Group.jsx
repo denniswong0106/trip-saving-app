00443d827ccb8746317c229b45944cb2bd9de62d
Ted <ted@ted-ubuntu-desktop>
2021-05-07 23:18:40 -0600

    add add friends in group trip page

--- react-front-end/src/components/Group.jsx
+++ react-front-end/src/components/Group.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import NavBar from './NavBar';
-import LinearProgressWithLabel from "./LinearProgressWithLabel";
+import LinearProgressWithLabel from "./helper_components/LinearProgressWithLabel";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+
 import "./Group.scss"
 
 const Group = (props) => {
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const today = new Date();
   const currentDay = today.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
+
+
 
   return(
     <>
@@ -25,7 +44,24 @@ const Group = (props) => {
         <h4>Rex Raptor</h4>
         <LinearProgressWithLabel value={75} />
       </div>
-
+      
+      <div>
+        <Fab size="large" color="primary" aria-label="add" onClick={handleClick}>
+          <AddIcon fontSize="large"/>
+        </Fab>
+        <Menu
+          id="fade-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          TransitionComponent={Fade}
+        >
+          <MenuItem onClick={handleClose}>Megan Mann</MenuItem>
+          <MenuItem onClick={handleClose}>Weavile Underwood</MenuItem>
+          <MenuItem onClick={handleClose}>Nema Toed</MenuItem>
+        </Menu>
+      </div>
     </>
   );
 }
