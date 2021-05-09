8b640525b93c43598735d0a824f46b2c369c3b91
Dennis Wong <denniswong0106@gmail.com>
2021-05-09 04:35:32 +0000

    add all seed files to work with new schema. Add new stretch_goal schema edit where if stretch_goal defaults to 0 if no input given

--- express-back-end/db/seeds/02_group_trip.js
+++ express-back-end/db/seeds/02_group_trip.js
@@ -4,6 +4,10 @@ exports.seed = function (knex) {
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("group_trip").insert([{ id: 1, name: "Iceland 2022!!!" }]);
+      return knex("group_trip").insert([
+        { id: 1, name: "Iceland 2022!!!" },
+        { id: 2, name: "Fireland 2023!!!" },
+        { id: 3, name: "Earthland 2024!!!" },
+      ]);
     });
 };
