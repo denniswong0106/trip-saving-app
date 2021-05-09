b15a49812a20a385148bebb994f12a54fd95e7d6
Dennis Wong <denniswong0106@gmail.com>
2021-05-09 04:08:23 +0000

    add daily_prize and booking_date into trip_savings table

---
+++ express-back-end/db/migrations/20210509040244_trips_add_date_and_daily_prize.js
@@ -0,0 +1,29 @@
+// add date and daily_prize to trips table
+
+exports.up = function (knex) {
+  return knex.schema.table("trip_savings", function (t) {
+    t.boolean("daily_prize").notNull().defaultTo(false);
+    t.date("booking_date").notNull();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table("trip_savings", function (t) {
+    t.dropColumn("daily_prize");
+    t.dropColumn("booking_date");
+  });
+};
+
+// {
+//   id: 1,
+//   savings: 100,
+//   daily_drip: 1,
+//   trip_name: "Ice Land Adventures",
+//   cost: 1400,
+//   location: "Ice Land",
+//   description: "It's cold",
+//   booking_date:
+//   daily_prize: true,
+//   user_id: 1,
+//   group_id: 1,
+// }
