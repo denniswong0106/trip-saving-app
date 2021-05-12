78682c5ba2ac050e56dab3ebeb863133ad0fb28b
P-Hansen <philip.pt.hansen@gmail.com>
2021-05-12 14:31:03 -0600

    added 'savings' and 'cost' to updates because numeric is recommended for monetary amounts

--- express-back-end/db/migrations/20210512202031_daily_drip_up.js
+++ express-back-end/db/migrations/20210512202031_daily_drip_up.js
@@ -2,11 +2,15 @@
 exports.up = function(knex) {
     return knex.schema.alterTable("trip_savings", (table) => {
         table.numeric("daily_drip").notNullable();
+        table.numeric("savings").notNullable();
+        table.numeric("cost").notNullable();
     });
 };
 
 exports.down = function(knex) {
     return knex.schema.alterTable("trip_savings", (table) => {
         table.dropColumn("daily_drip");
+        table.dropColumn("savings");
+        table.dropColumn("cost");
     });
 };
