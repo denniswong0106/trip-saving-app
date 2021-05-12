23a2ca2ab06a19d2d83150e6bed996991426b162
Dennis Wong <denniswong0106@gmail.com>
2021-05-12 04:28:41 +0000

    finish home components to use api data

--- react-front-end/src/components/Home_tripItemList.jsx
+++ react-front-end/src/components/Home_tripItemList.jsx
@@ -6,12 +6,21 @@ const TripItemList = (props) => {
   // uses useContext to grab the appropriate functions to use it instead of prop drilling
   // const { state, fetchData } = useContext(DataContext);
 
-  console.log("props", props);
-  return (
-    <div className="trip-container">
-      <TripItem />
-    </div>
-  );
+  // const newArray = props.trips.map((trip) => {
+  //   console.log(trip);
+  // });
+  const tripsArray = Array.from(props.trips);
+  const trips = tripsArray.map((trip) => {
+    return (
+      <TripItem
+        name={trip.name}
+        description={trip.description}
+        image={trip.images[2].image_href}
+      />
+    );
+  });
+
+  return <div className="trip-container">{trips}</div>;
 };
 
 export default TripItemList;
