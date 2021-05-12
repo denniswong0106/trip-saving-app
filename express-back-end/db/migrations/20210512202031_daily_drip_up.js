594b888239a01647cc0408ed7ff62ca7c15d45d0
P-Hansen <philip.pt.hansen@gmail.com>
2021-05-12 14:27:18 -0600

    new daily drip table 'numeric'

--- express-back-end/db/migrations/20210512202031_daily_drip_up.js
+++ express-back-end/db/migrations/20210512202031_daily_drip_up.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.alterTable("trip_savings", (table) => {
+        table.numeric("daily_drip").notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.alterTable("trip_savings", (table) => {
+        table.dropColumn("daily_drip");
+    });
 };
