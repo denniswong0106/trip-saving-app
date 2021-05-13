013042495fb027f7fc33c24df437cbd9a2653db4
Ted <ted@ted-ubuntu-desktop>
2021-05-13 12:17:16 -0600

    add minor bug fix

--- react-front-end/src/components/Home_tripItem.jsx
+++ react-front-end/src/components/Home_tripItem.jsx
@@ -8,20 +8,13 @@ const TripItem = (props) => {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className="trip-block">
-        <div className="text-and-heading">
-          <h3>{props.name}</h3>
-          <p>{props.description}</p>
-=======
       <Link to={`/trip/${props.id}`}>
-        <div class="trip-block">
+        <div className="trip-block">
           <div className="text-and-heading">
             <h3>{props.name}</h3>
             <p>{props.description}</p>
           </div>
           <img src={props.image} alt="waterfall" />
->>>>>>> ba405b43848cb88f0e3917b5a66ad28eba52aed6
         </div>
       </Link>
       <br />
--- react-front-end/src/components/Home_tripItemList.jsx
+++ react-front-end/src/components/Home_tripItemList.jsx
@@ -6,13 +6,6 @@ const TripItemList = (props) => {
   // uses useContext to grab the appropriate functions to use it instead of prop drilling
   // const { state, fetchData } = useContext(DataContext);
 
-<<<<<<< HEAD
-  // const newArray = props.trips.map((trip) => {
-  //   console.log(trip);
-  // });
-
-=======
->>>>>>> ba405b43848cb88f0e3917b5a66ad28eba52aed6
   const tripsArray = Array.from(props.trips);
   const trips = tripsArray.map((trip, index) => {
     return (
