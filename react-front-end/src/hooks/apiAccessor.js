f9e0c1d1d8768ea855758d22e1754547935b54ec
Ted <ted@ted-ubuntu-desktop>
2021-05-12 22:39:00 -0600

    add minor merge fix

--- react-front-end/src/hooks/apiAccessor.js
+++ react-front-end/src/hooks/apiAccessor.js
@@ -18,40 +18,13 @@ export default function apiAccessor() {
 
   // ---------------------------------------
 
-  // filter function that removes repeat titles, creates an array
-  // of ids of
-
-  const removeRepeats = (result) => {
-    const searchResults = result.data.results;
-    console.log("initial axios call data...", result.data.results);
-    let idArray = []; //pulls out the ids of each uniquely named trip object
-
-    searchResults.reduce((unique, item) => {
-      if (unique.includes(item.name)) {
-        return unique;
-      } else {
-        idArray = [...idArray, item.id];
-        return [...unique, item.name];
-      }
-    }, []);
-
-    return idArray;
-  };
-
-  // ----------------------------------------
-
   useEffect(() => {
     const firstUrl = `https://rest.gadventures.com/tour_dossiers/?name=${search}`;
     const secondUrl = `https://rest.gadventures.com/tour_dossiers/`;
 
-<<<<<<< HEAD
     // this callback takes in api call for ids, return an array of only ids
     const findSearchIds = (result) => {
       console.log(`search bar request to API: ${search}`, result.data.results);
-
-      const myArr = result.data.results;
-      console.log("myArr: ", myArr);
-
 
       // map through array, return only an array of ID's
       const idArray = result.data.results.map((tripObj) => {
@@ -60,11 +33,8 @@ export default function apiAccessor() {
       return idArray;
     };
 
-=======
->>>>>>> 8bf40f87d1ad9a9aa98eea1ee00ebc9320a02a6f
     // the first call handles will get ids, and create array of ids:
-    const firstCall = axios.get(firstUrl, header).then(removeRepeats);
-    // .then(findSearchIds);
+    const firstCall = axios.get(firstUrl, header).then(findSearchIds);
 
     // the second call will create the first 5 trip states.
     const secondCall = firstCall.then((result) => {
