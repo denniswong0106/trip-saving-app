b3c0328008d143c07099bebbe573f95fca0a9ef9
Ted <ted@ted-ubuntu-desktop>
2021-05-12 21:27:29 -0600

    add refactor of days calculation from Group.jsx to helperFunctions

--- react-front-end/src/components/Group.jsx
+++ react-front-end/src/components/Group.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import Menu from '@material-ui/core/Menu';
@@ -6,7 +6,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Fade from '@material-ui/core/Fade';
 import GroupItem from "./GroupItem";
 import DataContext from "../helperfunctions/DataContext";
-import { calculatePercentage } from "../helperfunctions/calculateFunctions";
+import { calculatePercentage, daysRemaining, currentDay } from "../helperfunctions/calculateFunctions";
 import axios from "axios";
 
 import "./Group.scss";
@@ -21,19 +21,8 @@ const Group = () => {
   const friendsList = getUsersIdByGroupId(1);
   const allUsers = getUsersIdNotInGroup(1);
 
-  
-  // grabs today's date and the trip date and calculates remaining days
-  const today = new Date();
-  const tripDay = new Date(trip.booking_date);
-  
-  const timeDiff = Math.abs(today - tripDay);
-  const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-  
-  // grabs today's date and converts it to a Month, Day, Year format
-  const currentDay = today.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
-  
   // state that the menu from material ui uses
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
 
   // click handler that the menu from material ui uses
@@ -89,7 +78,6 @@ const Group = () => {
     setAnchorEl(null);
   }
   
-  
   // maps through an array of users that are already in the group
   const groupFriendList = friendsList.map(friend => {
     
@@ -135,8 +123,8 @@ const Group = () => {
     <>
       <div className="group-title">
         <h1>{trip.trip_name}</h1>
-        <h2>Date: {currentDay}</h2>
-        <h4>Only {daysRemaining} days to go!</h4>
+        <h2>Date: {currentDay()}</h2>
+        <h4>Only {daysRemaining(trip.booking_date)} days to go!</h4>
       </div>
       <div>
         <h1>Progress:</h1>
