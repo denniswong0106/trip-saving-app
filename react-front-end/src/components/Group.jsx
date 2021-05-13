318129bc5c187bc99a7ac7a8c9da7e7b1a2d3cd8
Dennis Wong <denniswong0106@gmail.com>
2021-05-13 04:29:55 +0000

    fix for bug repeat trip objects from home page call

--- react-front-end/src/components/Group.jsx
+++ react-front-end/src/components/Group.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Fade from '@material-ui/core/Fade';
+import React, { useContext } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Fade from "@material-ui/core/Fade";
 import GroupItem from "./GroupItem";
 import DataContext from "../helperfunctions/DataContext";
 import { calculatePercentage } from "../helperfunctions/calculateFunctions";
@@ -11,25 +11,32 @@ import { calculatePercentage } from "../helperfunctions/calculateFunctions";
 import "./Group.scss";
 
 const Group = () => {
-
   // import DataContext functions
-  const { getUsersIdByGroupId, getTripByGroupAndUserId, getUsersIdNotInGroup } = useContext(DataContext);
+  const {
+    getUsersIdByGroupId,
+    getTripByGroupAndUserId,
+    getUsersIdNotInGroup,
+  } = useContext(DataContext);
 
   // assigns data to variables
-  const trip = {...getTripByGroupAndUserId(1, 1)};
+  const trip = { ...getTripByGroupAndUserId(1, 1) };
   const friendsList = getUsersIdByGroupId(1);
   const allUsers = getUsersIdNotInGroup(1);
- 
+
   // grabs today's date and the trip date and calculates remaining days
   const today = new Date();
   const tripDay = new Date(trip.booking_date);
-  
+
   const timeDiff = Math.abs(today - tripDay);
   const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-  
+
   // grabs today's date and converts it to a Month, Day, Year format
-  const currentDay = today.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'})
-  
+  const currentDay = today.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   // state that the menu from material ui uses
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -45,13 +52,12 @@ const Group = () => {
   };
 
   // maps through an array of users that are already in the group
-  const groupFriendList = friendsList.map(friend => {
-    
+  const groupFriendList = friendsList.map((friend) => {
     // grabs the specific trip the group is on
     const trip = getTripByGroupAndUserId(1, friend.id);
-    // calculates the progress 
+    // calculates the progress
     const progress = calculatePercentage(trip.savings, trip.cost);
-    
+
     return (
       <GroupItem
         key={friend.id}
@@ -63,7 +69,9 @@ const Group = () => {
   });
 
   // maps through an array of "friends" not yet added to the group
-  const addGroupFriendsList = allUsers.map(friend => <MenuItem onClick={handleClose}>{friend.name}</MenuItem>);
+  const addGroupFriendsList = allUsers.map((friend) => (
+    <MenuItem onClick={handleClose}>{friend.name}</MenuItem>
+  ));
 
   return (
     <>
