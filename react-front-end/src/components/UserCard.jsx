993f7f78d32730d58d0d861917bdf541daff8172
P-Hansen <philip.pt.hansen@gmail.com>
2021-05-14 17:23:10 -0600

    cleanup/refactoring

--- react-front-end/src/components/UserCard.jsx
+++ react-front-end/src/components/UserCard.jsx
@@ -10,25 +10,13 @@ const UserCard = () => {
 
     return (
         <div class="main-box">
-            <div class="bg-pattern-top">
-                <img src="./images/bg-pattern-top.svg" alt=""/>
-            </div>
-            <div class="bg-pattern-bottom">
-                <img src="./images/bg-pattern-bottom.svg" alt=""/>
-            </div>
-            <div>
-                {/* <img class="bg-pattern-card" src="https://media.istockphoto.com/vectors/aircraft-destinations-with-planes-icons-on-blue-background-abstract-vector-id1174854848?k=6&m=1174854848&s=612x612&w=0&h=g6jss7xxMSgBz9z2YCPvi_oxV6C3It24REb9wSnrBmM=" alt=""/> */}
-            </div>
-            <div>
-                <img class="image" src={user.avatar} alt=""/>
-            </div>
-            <div>
-                <h3>
-                    {user.name} <span class="age">26</span>
-                </h3>
-            </div>
-
-            <p>London</p>
+            <div className="bg-pattern-top" />
+            <img className="image" src={user.avatar} alt=""/>
+            <div className="bg-pattern-bottom"/>
+            <h3 className="name">
+                {user.name} <span class="age">26</span>
+            </h3>
+            <p className="location">London</p>
         </div>
     );
 };
