94fef1f7fc7801c6c21c727263552fadf8f6c5d5
P-Hansen <philip.pt.hansen@gmail.com>
2021-05-14 13:57:19 -0600

    basic style and transparent logos added

--- react-front-end/src/components/NavBar.jsx
+++ react-front-end/src/components/NavBar.jsx
@@ -4,20 +4,20 @@ import "./NavBar.scss";
 import Button from "@material-ui/core/Button";
 
 const NavBar = (props) => {
+
+  
+
   return (
     <nav className="nav-bar">
       <div className="trickle-logo">
         <Link to="/">
-          <Button color="primary">Trickle Trip </Button>
+          <img src={require("../pics/piggyAssets/2transparent.png")} alt="pic" ></img>
         </Link>
+        <p>Trickle Trip</p>
       </div>
       <div className="login-trips">
-        <Link to="/user/1">
-          <Button color="primary">Login</Button>
-        </Link>
-        <Link to="/trip">
-          <Button color="primary">Trips</Button>
-        </Link>
+        <Button onClick={(e)=>{e.preventDefault(); window.location.href="/"}}>Logout</Button>
+        <Button onClick={(e)=>{e.preventDefault(); window.location.href="/user/1"}}>My Trips</Button>
       </div>
     </nav>
   );
