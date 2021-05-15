ee549db5d06b6103d675ad9ecd1f33a65d6848d6
Ted <ted@ted-ubuntu-desktop>
2021-05-15 14:04:10 -0600

    add up-arrow gif to make go to top button more legible

--- react-front-end/src/components/helper_components/ScrollToTopButton.jsx
+++ react-front-end/src/components/helper_components/ScrollToTopButton.jsx
@@ -30,7 +30,8 @@ const ScrollToTopButton = () => {
     <div className="scroll-to-top-button">
       {isVisible && 
         <div onClick={scrollToTop}>
-          <img className="scroll-to-top-button--scroll-img" src='https://github.com/denniswong0106/trip-saving-app/blob/master/react-front-end/src/pics/piggyAssets/16transparent.png?raw=true' alt='Go to top'/>
+          <img className="scroll-to-top-button--arrow-img" src='https://media2.giphy.com/media/LmHEzN3V3gFpQu8Vm0/giphy.gif?cid=790b76110fdeda5290d920700020e1c4677f5fe87ebce58b&rid=giphy.gif&ct=s' alt='up-arrow'/>
+          <img className="scroll-to-top-button--scroll-img" src='https://github.com/denniswong0106/trip-saving-app/blob/master/react-front-end/src/pics/piggyAssets/16transparent.png?raw=true' alt='Go-to-top'/>
           <h3>Go to top</h3>
         </div>}
     </div>
