4f7f4ac048fc4717962e26115062990733899999
Ted <ted@ted-ubuntu-desktop>
2021-05-18 22:53:39 -0600

    add change to seeds and change surpriseMechanic function to result to a static prize

--- express-back-end/db/seeds/02_group_trip.js
+++ express-back-end/db/seeds/02_group_trip.js
@@ -5,9 +5,8 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("group_trip").insert([
-        { id: 1, name: "Iceland 2022!!!" },
-        { id: 2, name: "Fireland 2023!!!" },
-        { id: 3, name: "Earthland 2024!!!" },
+        { id: 1, name: "Iceland 2077 !!" },
+
       ]);
     });
 };
